12ba9c48a6a0cb3558bcfd64ea63cacc0546595f
liuYukun260 <2528075388@qq.com>
2025-05-27 13:56:32 +0800

    timerform界面优化

--- TomatoTaskDemo副本/Views/TimerForm.Designer.cs
+++ TomatoTaskDemo副本/Views/TimerForm.Designer.cs
@@ -45,9 +45,9 @@
             // 
             lblTime.Font = new Font("Ink Free", 39.9999962F, FontStyle.Regular, GraphicsUnit.Point, 0);
             lblTime.ForeColor = SystemColors.ControlDarkDark;
-            lblTime.Location = new Point(12, 123);
+            lblTime.Location = new Point(12, 118);
             lblTime.Name = "lblTime";
-            lblTime.Size = new Size(403, 181);
+            lblTime.Size = new Size(403, 174);
             lblTime.TabIndex = 0;
             lblTime.Text = "00:00:00";
             lblTime.TextAlign = ContentAlignment.MiddleCenter;
@@ -63,21 +63,22 @@
             splitContainer1.Panel1.Controls.Add(btnChangeTime);
             splitContainer1.Panel1.Controls.Add(panel1);
             splitContainer1.Panel1.Controls.Add(lblTime);
+            splitContainer1.Panel1MinSize = 325;
             // 
             // splitContainer1.Panel2
             // 
             splitContainer1.Panel2.Controls.Add(lblTotalTime);
             splitContainer1.Panel2.Controls.Add(btnStop);
             splitContainer1.Panel2.Controls.Add(btnStart);
-            splitContainer1.Size = new Size(800, 469);
-            splitContainer1.SplitterDistance = 322;
+            splitContainer1.Size = new Size(960, 450);
+            splitContainer1.SplitterDistance = 440;
             splitContainer1.TabIndex = 0;
             // 
             // btnChangeTime
             // 
             btnChangeTime.Location = new Point(116, 11);
             btnChangeTime.Name = "btnChangeTime";
-            btnChangeTime.Size = new Size(141, 32);
+            btnChangeTime.Size = new Size(141, 31);
             btnChangeTime.TabIndex = 2;
             btnChangeTime.Text = "切换为模拟钟";
             btnChangeTime.UseVisualStyleBackColor = true;
@@ -86,27 +87,27 @@
             // panel1
             // 
             panel1.ForeColor = SystemColors.GrayText;
-            panel1.Location = new Point(15, 52);
+            panel1.Location = new Point(15, 50);
             panel1.Name = "panel1";
-            panel1.Size = new Size(400, 417);
+            panel1.Size = new Size(400, 400);
             panel1.TabIndex = 1;
             panel1.Visible = false;
             // 
             // lblTotalTime
             // 
             lblTotalTime.Font = new Font("Microsoft YaHei UI", 15F);
-            lblTotalTime.Location = new Point(3, 269);
+            lblTotalTime.Location = new Point(37, 253);
             lblTotalTime.Name = "lblTotalTime";
-            lblTotalTime.Size = new Size(345, 57);
+            lblTotalTime.Size = new Size(345, 55);
             lblTotalTime.TabIndex = 2;
             lblTotalTime.Text = "总用时:0";
             lblTotalTime.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // btnStop
             // 
-            btnStop.Location = new Point(196, 123);
+            btnStop.Location = new Point(261, 118);
             btnStop.Name = "btnStop";
-            btnStop.Size = new Size(121, 59);
+            btnStop.Size = new Size(121, 57);
             btnStop.TabIndex = 1;
             btnStop.Text = "停止计时";
             btnStop.UseVisualStyleBackColor = true;
@@ -114,9 +115,9 @@
             // 
             // btnStart
             // 
-            btnStart.Location = new Point(30, 123);
+            btnStart.Location = new Point(30, 118);
             btnStart.Name = "btnStart";
-            btnStart.Size = new Size(129, 59);
+            btnStart.Size = new Size(129, 57);
             btnStart.TabIndex = 0;
             btnStart.Text = "开始计时";
             btnStart.UseVisualStyleBackColor = true;
@@ -124,9 +125,9 @@
             // 
             // TimerForm
             // 
-            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleDimensions = new SizeF(11F, 24F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 469);
+            ClientSize = new Size(960, 450);
             Controls.Add(splitContainer1);
             Name = "TimerForm";
             Text = "TimerForm";
